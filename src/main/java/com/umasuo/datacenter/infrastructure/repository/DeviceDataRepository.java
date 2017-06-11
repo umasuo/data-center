4382c308a958d23ecb3fb7e42c5244cdc5653ee6
umasuo <liuquan89@gmail.com>
2017-06-11 19:37:46 +0800

    refactor

--- src/main/java/com/umasuo/datacenter/infrastructure/repository/DeviceDataRepository.java
+++ src/main/java/com/umasuo/datacenter/infrastructure/repository/DeviceDataRepository.java
@@ -2,14 +2,12 @@ package com.umasuo.datacenter.infrastructure.repository;
 
 import com.umasuo.datacenter.domain.model.DeviceData;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 /**
  * Created by umasuo on 17/2/10.
  */
 @Repository
-public interface DeviceDataRepository extends JpaRepository<DeviceData, String>,
-    CrudRepository<DeviceData, String> {
+public interface DeviceDataRepository extends JpaRepository<DeviceData, String> {
 
 }
