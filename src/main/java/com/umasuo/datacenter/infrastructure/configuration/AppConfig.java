bb9021846cf1e59046e3f40a3593aefd57003d43
umasuo <liuquan89@gmail.com>
2017-06-30 15:01:11 +0800

    add message support

---
+++ src/main/java/com/umasuo/datacenter/infrastructure/configuration/AppConfig.java
@@ -0,0 +1,38 @@
+package com.umasuo.datacenter.infrastructure.configuration;
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by umasuo on 17/6/27.
+ */
+@Configuration
+@Data
+public class AppConfig {
+
+  /**
+   * message broker's host.
+   */
+  @Value("${message-broker.host:127.0.0.1}")
+  public String msgBrokerHost;
+
+  /**
+   * message broker's port
+   */
+  @Value("${message-broker.port:1883}")
+  public long msgBrokerPort;
+
+  /**
+   * super user's username for message broker.
+   */
+  @Value("${message-broker.username:umasuo}")
+  public String username;
+
+  /**
+   * super user's password for message broker.
+   */
+  @Value("${message-broker.password:password}")
+  public String password;
+
+}
